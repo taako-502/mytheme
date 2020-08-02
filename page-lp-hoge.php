2804bf05c5d688b73b1f197fb40eb7c7b31deefe
taako <a@a>
2020-08-02 23:55:28 +0900

    固定ページとランディングページ（hoge）追加

---
+++ page-lp-hoge.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<section id="content">
+<div id="content-wrap" class="container">
+<div id="main" class="">
+  <h1>テスト</h1>
+  <h2>テスト１</h2>
+  <h3>あああ</h3>
+  <h2>テスト２</h2>
+  <h2>テスト３</h2>
+</div>
+<div id="sidebar" class="col-md-3">
+<?php get_sidebar(); ?>
+</div>
+</div>
+</section>
+<?php wp_footer(); ?>
