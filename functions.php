9baa3a3a90319af6cf792f90198a24d84e4a6695
taako <a@a>
2020-08-05 00:03:31 +0900

    ダッシュボードにウィジェット追加

--- functions.php
+++ functions.php
@@ -1,10 +1,34 @@
 <?php
+/**
+* テーマのセットアップメソッド
+*/
 function mytheme_setup(){
   // アイキャッチ画像をON
   add_theme_support('post-thumbnails');
   // メニュー機能をON
   add_theme_support('menus');
 }
+
+/**
+* 追加ウィジェットの実装
+*/
+function add_custom_widget(){
+  echo '<div class="custom_widget"><p>Hello World</p></div>';
+}
+
+/**
+* ダッシュボードにウィジェット追加
+*/
+function add_widget(){
+  wp_add_dashboard_widget(
+    'custom_widget',
+    'テーマ設定',
+    'add_custom_widget');
+}
+
+// フック処理
 add_action('after_setup_theme','mytheme_setup');
+add_action('wp_dashboard_setup','add_widget');
+// 記事の冒頭に表示するアイキャッチ画像
 add_image_size('single',800,450,false);
 ?>
