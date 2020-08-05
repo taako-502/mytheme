30b35c5ea7852b9110024673d63d67b8b04f1cd5
taako <a@a>
2020-08-05 21:22:54 +0900

    管理画面にメニュー追加

--- functions.php
+++ functions.php
@@ -10,10 +10,27 @@ function mytheme_setup(){
 }
 
 /**
-* 追加ウィジェットの実装
+* 管理メニューに管理画面追加
 */
-function add_custom_widget(){
-  echo '<div class="custom_widget"><p>Hello World</p></div>';
+function add_admin(){
+  add_menu_page(
+    'mythemeの簡単設定',
+    'mytheme設定',
+    'manage_options',
+    'mytheme-admin',
+    'add_custom_admin',
+    'dashicons-store',
+    59);
+}
+
+/**
+ * 追加管理画面の実装
+ */
+function add_custom_admin(){
+  // mytheme専用管理画面呼び出し
+  if (locate_template('/mytheme-admin.php') !== '') {
+    require_once locate_template('/mytheme-admin.php');
+  }
 }
 
 /**
@@ -26,9 +43,18 @@ function add_widget(){
     'add_custom_widget');
 }
 
+/**
+* 追加ウィジェットの実装
+*/
+function add_custom_widget(){
+  echo '<div class="custom_widget"><p>Hello World</p></div>';
+}
+
 // フック処理
 add_action('after_setup_theme','mytheme_setup');
 add_action('wp_dashboard_setup','add_widget');
+add_action('admin_menu','add_admin');
 // 記事の冒頭に表示するアイキャッチ画像
 add_image_size('single',800,450,false);
+
 ?>
