627d96ecc384b7c02633c619b7df70269acdecd4
taako-502 <i.am.armorknight@gmail.com>
2020-08-09 03:10:31 +0900

    コメント追加

--- dbutil.php
+++ dbutil.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * wp_optionsテーブルの設定値を更新
+ * @param  [type] $key       キー
+ * @param  [type] $post_data 値
+ * @return [type]            更新値
+ */
 function updaeteOptionPost($key,$post_data){
   if ( isset( $post_data ) && $post_data ) {
     update_option( $key, $post_data );
