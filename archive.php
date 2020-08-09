38c1e95364e3526bf271bc2fd1043e7ca07dfb55
taako-502 <i.am.armorknight@gmail.com>
2020-08-10 03:12:16 +0900

    ブログカードをflexboxで綺麗に配置

--- archive.php
+++ archive.php
@@ -5,62 +5,28 @@
       <h1>記事一覧</h1>
       <hr>
       <div class="article-list">
-        <div class="left-article-list">
-          <?php
+        <?php
           /* 記事一覧左側 */
           if ( have_posts() ) {
             while ( have_posts() ) { the_post();
-              if ($wp_query->current_post % 2 == 1){
-                // 奇数ページはcontinue
-                continue;
-              }
-          ?>
-          <div class="article-card">
-            <?php
-                if ( has_post_thumbnail() ) {
-                  the_post_thumbnail('articlelist');
-                }
-                else {
-                  echo '<img width="288" height="162" src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/thumbnail-default.jpg" />';
-                }
-            ?>
-            <h2>
-              <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-            </h2>
-          </div>
-        <?php
-            }
-          }
         ?>
-        </div>
-        <div class="right-article-list">
+        <div class="article-card">
           <?php
-          /* 記事一覧右側 */
-          if ( have_posts() ) {
-            while ( have_posts() ) { the_post();
-                // 偶数ページは飛ばす
-                if ($wp_query->current_post % 2 == 0){
-                continue;
+              if ( has_post_thumbnail() ) {
+                the_post_thumbnail('articlelist');
+              }
+              else {
+                echo '<img width="288" height="162" src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/thumbnail-default.jpg" />';
               }
           ?>
-          <div class="article-card">
-            <?php
-                if ( has_post_thumbnail() ) {
-                  the_post_thumbnail('articlelist');
-                }
-                else {
-                  echo '<img width="288" height="162" src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/thumbnail-default.jpg" />';
-                }
-            ?>
-            <h2>
-              <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-            </h2>
-          </div>
+          <h2>
+            <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+          </h2>
+        </div>
         <?php
             }
           }
         ?>
-        </div>
       </div>
     </div>
     <div id="sidebar" class="col-md-3">
