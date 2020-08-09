30b1f600a410645a69f5c411f3ede6523ac22db4
taako-502 <i.am.armorknight@gmail.com>
2020-08-10 01:27:27 +0900

    記事一覧をブログカード風に変更

--- archive.php
+++ archive.php
@@ -1,33 +1,69 @@
 <?php get_header(); ?>
 <section id="content">
-<div id="content-wrap" class="container">
-<div id="main" class="col-md-9" >
-  <h1>記事一覧</h1>
-  <hr>
-  <?php
-    if ( have_posts() ) :
-      while ( have_posts() ) : the_post();
-  ?>
-      <h2>
-        <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-      </h2>
-  <?php
-        if ( has_post_thumbnail() ) {
-          the_post_thumbnail('articlelist');
-        }
-        else {
-          echo '<img width="288" height="162" src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/thumbnail-default.jpg" />';
-        }
-  ?>
-    <hr>
-  <?php
-      endwhile;
-    endif;
-  ?>
-</div>
-<div id="sidebar" class="col-md-3">
-<?php get_sidebar(); ?>
-</div>
-</div>
+  <div id="content-wrap" class="container">
+    <div id="main" class="col-md-9" >
+      <h1>記事一覧</h1>
+      <hr>
+      <div class="article-list">
+        <div class="left-article-list">
+          <?php
+          /* 記事一覧左側 */
+          if ( have_posts() ) {
+            while ( have_posts() ) { the_post();
+              if ($wp_query->current_post % 2 == 1){
+                // 奇数ページはcontinue
+                continue;
+              }
+          ?>
+          <h2>
+            <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+          </h2>
+          <?php
+              if ( has_post_thumbnail() ) {
+                the_post_thumbnail('articlelist');
+              }
+              else {
+                echo '<img width="288" height="162" src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/thumbnail-default.jpg" />';
+              }
+          ?>
+          <hr>
+        <?php
+            }
+          }
+        ?>
+        </div>
+        <div class="right-article-list">
+          <?php
+          /* 記事一覧右側 */
+          if ( have_posts() ) {
+            while ( have_posts() ) { the_post();
+                // 偶数ページは飛ばす
+                if ($wp_query->current_post % 2 == 0){
+                continue;
+              }
+          ?>
+          <h2>
+            <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+          </h2>
+          <?php
+              if ( has_post_thumbnail() ) {
+                the_post_thumbnail('articlelist');
+              }
+              else {
+                echo '<img width="288" height="162" src="' . get_bloginfo( 'stylesheet_directory' ) . '/images/thumbnail-default.jpg" />';
+              }
+          ?>
+          <hr>
+        <?php
+            }
+          }
+        ?>
+        </div>
+      </div>
+    </div>
+    <div id="sidebar" class="col-md-3">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
 </section>
 <?php wp_footer(); ?>
