4dbdbb4bb86345577505f25ba166a4bb07f8eb6e
taako-502 <i.am.armorknight@gmail.com>
2020-08-10 16:42:24 +0900

    cssフォルダ作成

---
+++ header-top.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <meta charset="utf-8">
+ <title>mytheme</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/common.css">
+ <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/index.css">
+ <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/mythemecss.php" />
+ <?php wp_enqueue_script('jquery'); ?>
+ <?php wp_enqueue_script('bootstrap-js','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',array('jquery')); ?>
+ <?php wp_head(); ?>
+</head>
+<body>
+  <header>
+  <nav class="navbar navbar-default" style="margin-bottom:0;">
+    <div id="title">
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+    </div>
+    <div id="bs-navbar-collapse-1" class="collapse navbar-collapse">
+      <?php
+        $args = array(
+          'menu_class' => 'nav navbar-nav' ,
+          'container' => false,
+        );
+        wp_nav_menu($args);
+       ?>
+    </div>
+  </nav>
--- home.php
+++ home.php
@@ -1,11 +1,24 @@
-<?php get_header(); ?>
+<?php get_header('top'); ?>
+<div class="topimage">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/top-default.jpg" alt="トップページ画像" >
+</div>
 <section id="content">
   <div id="content-wrap" class="container">
-    <div id="main" class="col-md-9">
-      <h1 class="text-center">個人事業主向けWebスクール ウェブストエイト</h1>
+    <div id="main" class="col-md-12">
+      <h1 class="text-center">TAAKO作成オリジナルテーマ</h1>
+      <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+      <h2>クールで、エキサイティングな体験を。</h2>
+      <p>使い方はこちらで解説。</p>
+      <input type="button" name="how-to-mytheme" value="MYTHEMEの使い方">
     </div>
-    <div id="sidebar" class="col-md-3">
-      <?php get_sidebar(); ?>
+    <div id="recommend-left" class="col-md-4">
+      hoge
+    </div>
+    <div id="recommend-center" class="col-md-4">
+      fuga
+    </div>
+    <div id="recommend-right" class="col-md-4">
+      piyo
     </div>
   </div>
 </section>
