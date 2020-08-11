3f79767fc35123677d66c70bbab887020b207b88
taako-502 <i.am.armorknight@gmail.com>
2020-08-11 14:55:30 +0900

    トップページにおすすめ記事追加

--- home.php
+++ home.php
@@ -1,24 +1,38 @@
 <?php get_header('top'); ?>
-<div class="topimage">
-  <img src="<?php echo get_template_directory_uri(); ?>/images/top-default.jpg" alt="トップページ画像" >
-</div>
+<div class="topimage"></div>
 <section id="content">
   <div id="content-wrap" class="container">
-    <div id="main" class="col-md-12">
-      <h1 class="text-center">TAAKO作成オリジナルテーマ</h1>
-      <p>testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
-      <h2>クールで、エキサイティングな体験を。</h2>
-      <p>使い方はこちらで解説。</p>
-      <input type="button" name="how-to-mytheme" value="MYTHEMEの使い方">
+    <div class="col-md-12">
+      <h2>おすすめ記事</h2>
+      <div class="recommend-article col-md-12">
+        <div class="recommend-left">
+          <a href="http://test.local/length-test/"><img src="<?php echo get_template_directory_uri(); ?>/images/thumbnail-default.jpg" alt="hoge"></a>
+        </div>
+        <div class="recommend-center">
+          <a href="http://test.local/hello-world/"><img src="<?php echo get_template_directory_uri(); ?>/images/thumbnail-default.jpg" alt="fuga"></a>
+        </div>
+        <div class="recommend-right">
+          <a href="http://test.local/hoge/"><img src="<?php echo get_template_directory_uri(); ?>/images/thumbnail-default.jpg" alt="piyo"></a>
+        </div>
+      </div>
     </div>
-    <div id="recommend-left" class="col-md-4">
-      hoge
+    <div id="main" class="col-md-9">
+      <h2>新着記事</h2>
+      <div class="article-list-one-column col-md-12">
+        <?php
+          /* 記事一覧 */
+          if ( have_posts() ) {
+            while ( have_posts() ) { the_post();
+              include(get_template_directory() ."/articlecard.php");
+            }
+          } else {
+            echo "<p>まだ記事がありません。</p>";
+          }
+        ?>
+      </div>
     </div>
-    <div id="recommend-center" class="col-md-4">
-      fuga
-    </div>
-    <div id="recommend-right" class="col-md-4">
-      piyo
+    <div id="sidebar" class="col-md-3">
+      <?php get_sidebar(); ?>
     </div>
   </div>
 </section>
