f8ae707e9a5327c1cf64edcc34456ac6d168f915
taako-502 <i.am.armorknight@gmail.com>
2020-08-12 01:48:53 +0900

    投稿ページにカテゴリー追加

--- single.php
+++ single.php
@@ -5,9 +5,12 @@
 <section id="content">
 <div id="content-wrap" class="container">
 <div id="main" class="col-md-9 entry-content" >
-<?php breadcrumb(); ?>
-  <?php the_post_thumbnail('single'); ?>
   <?php
+    breadcrumb();
+    $category_name = get_the_category()[0]->cat_name;
+    $category_link = get_category_link(get_the_category()[0]->cat_ID);
+    echo "<div class=\"category\"><a href=\"$category_link\">".$category_name."</a></div>";
+    the_post_thumbnail('single');
     if ( have_posts() ) :
       while ( have_posts() ) : the_post();
   ?>
