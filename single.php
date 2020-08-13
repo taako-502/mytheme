b4304c86be9ac24e9efc8f07c591855e02ee8044
taako-502 <i.am.armorknight@gmail.com>
2020-08-13 23:40:16 +0900

    記事で、スマホだとサムネイルの縦横比が崩れる問題を解消

--- single.php
+++ single.php
@@ -7,10 +7,12 @@
     <div class="row">
       <div id="main" class="col-md-9 entry-content" >
         <?php
+          // パンくずリスト
           breadcrumb();
           $category_name = get_the_category()[0]->cat_name;
           $category_link = get_category_link(get_the_category()[0]->cat_ID);
           echo "<div class=\"category\"><a href=\"$category_link\">".$category_name."</a></div>";
+          // サムネイル
           the_post_thumbnail('single');
           if ( have_posts() ) :
             while ( have_posts() ) : the_post();
