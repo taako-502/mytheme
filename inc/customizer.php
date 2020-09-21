90a875ae8895bf52b9f5c74ca532f93c0ca105f5
taako-502 <i.am.armorknight@gmail.com>
2020-09-21 15:11:08 +0900

    - インデント修正 - メニュー名修正

--- inc/customizer.php
+++ inc/customizer.php
@@ -4,3 +4,36 @@
  *
  * @package mytheme
  */
+function my_customize( $wp_customize ) {
+  $wp_customize->add_panel(
+    'my_panel',
+    array(
+      'title'    => 'ナビゲーションメニュー',
+      'priority' => 21,
+    )
+  );
+
+  $wp_customize->add_section(
+    'my_section',
+    array(
+      'title'    => 'カラー',
+      'panel'    => 'my_panel',
+      'priority' => 1,
+    )
+  );
+
+  $wp_customize->add_setting( 'my_setting' );
+  $wp_customize->add_control(
+    new WP_Customize_Control(
+      $wp_customize,
+      'my_control',
+      array(
+        'label'    => 'コントロールの見出し',
+        'section'  => 'my_section',
+        'settings' => 'my_setting',
+        'priority' => 1,
+      )
+    )
+  );
+}
+add_action( 'customize_register', 'my_customize' );
