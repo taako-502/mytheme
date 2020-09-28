5cec4ad630fde63dcc289ad643808c8f1d5bb1bd
taako-502 <i.am.armorknight@gmail.com>
2020-09-28 14:11:05 +0900

    タブレットやスマホの文字サイズを変更するプログラム追加

--- admin/admin.php
+++ admin/admin.php
@@ -2,7 +2,7 @@
 //初期処理
 include "admin_init.php";
 ?>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/admin.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/admin/admin.css">
 <div class="main">
   <h1>mythemeカスタマイズ画面</h1>
   <h2>画面説明</h2>
@@ -10,11 +10,34 @@ include "admin_init.php";
   <h2>基本構成</h2>
   <form method="post">
     <div class="setting">
+      <h3>PC閲覧時</h3>
       <p class="fontsize"><b>pタグ&ensp;&emsp;</b><input type="number" name="pc-p-size" value="<?php echo $pc_psize; ?>"> px</p>
       <p class="fontsize"><b>h1タグ&emsp;</b><input type="number" name="pc-h1-size" value="<?php echo $pc_h1size; ?>"> px</p>
       <p class="fontsize"><b>h2タグ&emsp;</b><input type="number" name="pc-h2-size" value="<?php echo $pc_h2size; ?>"> px</p>
       <p class="fontsize"><b>h3タグ&emsp;</b><input type="number" name="pc-h3-size" value="<?php echo $pc_h3size; ?>"> px</p>
       <p class="fontsize"><b>h4タグ&emsp;</b><input type="number" name="pc-h4-size" value="<?php echo $pc_h4size; ?>"> px</p>
+      <p class="fontsize"><b>h5タグ&emsp;</b><input type="number" name="pc-h5-size" value="<?php echo $pc_h5size; ?>"> px</p>
+      <p class="fontsize"><b>h6タグ&emsp;</b><input type="number" name="pc-h6-size" value="<?php echo $pc_h6size; ?>"> px</p>
+    </div>
+    <div class="setting">
+      <h3>タブレット閲覧時</h3>
+      <p class="fontsize"><b>pタグ&ensp;&emsp;</b><input type="number" name="tb-p-size" value="<?php echo $tb_psize; ?>"> px</p>
+      <p class="fontsize"><b>h1タグ&emsp;</b><input type="number" name="tb-h1-size" value="<?php echo $tb_h1size; ?>"> px</p>
+      <p class="fontsize"><b>h2タグ&emsp;</b><input type="number" name="tb-h2-size" value="<?php echo $tb_h2size; ?>"> px</p>
+      <p class="fontsize"><b>h3タグ&emsp;</b><input type="number" name="tb-h3-size" value="<?php echo $tb_h3size; ?>"> px</p>
+      <p class="fontsize"><b>h4タグ&emsp;</b><input type="number" name="tb-h4-size" value="<?php echo $tb_h4size; ?>"> px</p>
+      <p class="fontsize"><b>h5タグ&emsp;</b><input type="number" name="tb-h5-size" value="<?php echo $tb_h5size; ?>"> px</p>
+      <p class="fontsize"><b>h6タグ&emsp;</b><input type="number" name="tb-h6-size" value="<?php echo $tb_h6size; ?>"> px</p>
+    </div>
+    <div class="setting">
+      <h3>スマートフォン閲覧時</h3>
+      <p class="fontsize"><b>pタグ&ensp;&emsp;</b><input type="number" name="sm-p-size" value="<?php echo $sm_psize; ?>"> px</p>
+      <p class="fontsize"><b>h1タグ&emsp;</b><input type="number" name="sm-h1-size" value="<?php echo $sm_h1size; ?>"> px</p>
+      <p class="fontsize"><b>h2タグ&emsp;</b><input type="number" name="sm-h2-size" value="<?php echo $sm_h2size; ?>"> px</p>
+      <p class="fontsize"><b>h3タグ&emsp;</b><input type="number" name="sm-h3-size" value="<?php echo $sm_h3size; ?>"> px</p>
+      <p class="fontsize"><b>h4タグ&emsp;</b><input type="number" name="sm-h4-size" value="<?php echo $sm_h4size; ?>"> px</p>
+      <p class="fontsize"><b>h5タグ&emsp;</b><input type="number" name="sm-h5-size" value="<?php echo $sm_h5size; ?>"> px</p>
+      <p class="fontsize"><b>h6タグ&emsp;</b><input type="number" name="sm-h6-size" value="<?php echo $sm_h6size; ?>"> px</p>
     </div>
     <div class="setting">
       <h2>トップ</h2>
