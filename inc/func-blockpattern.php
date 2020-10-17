885479eebe649b6c8cc456c3e8c935605076c8fd
taako <36348377+taako-502@users.noreply.github.com>
2020-10-17 23:46:52 +0900

    デザイン追加

--- inc/func-blockpattern.php
+++ inc/func-blockpattern.php
@@ -4,14 +4,35 @@
  *
  * @package mytheme
  */
- $pattern = '<!-- wp:heading --><h2>見出し</h2><!-- /wp:heading -->
- 		<!-- wp:paragraph --><p>ブロックだよ</p><!-- /wp:paragraph -->';
+ $pattern = '<!-- wp:group -->
+<div class="wp-block-group p-student-voice"><div class="wp-block-group__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":64,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="http://test.local/wp-content/uploads/2020/10/book_idol_poster_woman.png" alt="" class="wp-image-64"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":18}}} -->
+<p style="font-size:18px">{生徒の名前}</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3>&lt;感想></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>{生徒の声：〇〇さんとの料理教室は、とても楽しかったです！また行きたい！}</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:group -->';
 
  register_block_pattern(
  	'my-pattern/test',
  	array(
- 		'title'       => 'テストパターン',
-     'description' => '見出しブロックと段落ブロックのセット',
+ 		'title'       => '生徒様の声ページ',
+     'description' => '顔写真、名前、感想の３点セット',
  		'content'     => $pattern,
  	)
  );
