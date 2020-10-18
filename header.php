2c4719b3c03a256f936e11401a6b5397096b528f
taako <36348377+taako-502@users.noreply.github.com>
2020-10-18 20:16:22 +0900

    フェイスブックのOGP設定

--- header.php
+++ header.php
@@ -2,18 +2,66 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
- <meta charset="utf-8">
- <title>mytheme</title>
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
- <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
- <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/common.css">
- <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/index.css">
- <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/mythemecss.php" />
- <?php wp_enqueue_script('jquery'); ?>
- <?php wp_enqueue_script('bootstrap-js','https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',array('jquery')); ?>
- <?php wp_head(); ?>
- <?php include ("template-parts/analyticsTracking.php"); ?>
+  <meta charset="utf-8">
+  <title>mytheme</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/common.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/index.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/mythemecss.php" />
+  <?php wp_enqueue_script('jquery'); ?>
+  <?php wp_enqueue_script('bootstrap-js','https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',array('jquery')); ?>
+  <?php wp_head(); ?>
+  <?php include ("template-parts/analyticsTracking.php"); ?>
+  <!-- ここからOGP -->
+  <meta property="og:type" content="blog">
+  <?php
+  if (is_single()){
+    //単一記事ページの場合
+    if(have_posts()): while(have_posts()): the_post();
+      //抜粋を表示
+      echo '<meta property="og:description" content="'.get_post_meta($post->ID, _aioseop_description, true).'">';echo "\n";
+    endwhile; endif;
+    //単一記事タイトルを表示
+    echo '<meta property="og:title" content="'; the_title(); echo '">';echo "\n";
+    //単一記事URLを表示
+    echo '<meta property="og:url" content="'; the_permalink(); echo '">';echo "\n";
+  } else {
+    //単一記事ページページ以外の場合（アーカイブページやホームなど）
+    //「一般設定」管理画面で指定したブログの説明文を表示
+    echo '<meta property="og:description" content="'; bloginfo('description'); echo '">';echo "\n";
+    //「一般設定」管理画面で指定したブログのタイトルを表示
+    echo '<meta property="og:title" content="'; bloginfo('name'); echo '">';echo "\n";
+    //「一般設定」管理画面で指定したブログのURLを表示
+    echo '<meta property="og:url" content="'; bloginfo('url'); echo '">';echo "\n";
+  }
+  $str = $post->post_content;
+  //投稿にイメージがあるか調べる
+  $searchPattern = '/<img.*?src=(["\'])(.+?)\1.*?>/i';
+  if (is_single()){
+    //単一記事ページの場合
+    if (has_post_thumbnail()){
+      //投稿にサムネイルがある場合の処理
+      $image_id = get_post_thumbnail_id();
+      $image = wp_get_attachment_image_src( $image_id, 'full');
+      echo '<meta property="og:image" content="'.$image[0].'">';echo "\n";
+    } else if ( preg_match( $searchPattern, $str, $imgurl ) && !is_archive()) {
+      //投稿にサムネイルは無いが画像がある場合の処理
+      echo '<meta property="og:image" content="'.$imgurl[2].'">';echo "\n";
+    } else {
+      //投稿にサムネイルも画像も無い場合の処理
+      echo '<meta property="og:image" content="画像URL">';echo "\n";
+    }
+  } else {
+    //単一記事ページページ以外の場合（アーカイブページやホームなど）
+    echo '<meta property="og:image" content="画像URL">';echo "\n";
+  }
+  ?>
+  <meta property="og:site_name" content="<?php bloginfo('name'); ?>">
+  <meta property="fb:admins" content="管理者ID">
+  <meta property="fb:app_id" content="アプリID">
+  <!-- ここまでOGP -->
 </head>
 <body>
   <header>
