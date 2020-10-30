be7024f05f3f007f9767cde9d0608fc549bb8a97
taako <36348377+taako-502@users.noreply.github.com>
2020-10-31 00:46:45 +0900

    関連記事に画像を表示するよう修正

--- template-parts/content-related.php
+++ template-parts/content-related.php
@@ -1,28 +1,38 @@
 <aside class="relatedkiji">
   <h4>関連記事</h4>
   <ul>
-  <?php if(has_category() ) {
-    $cats =get_the_category();
-    $catkwds = array();
-    foreach($cats as $cat){
-         $catkwds[] = $cat->term_id;
+    <?php
+    if(has_category() ) {
+      $cats =get_the_category();
+      $catkwds = array();
+      foreach($cats as $cat){
+        $catkwds[] = $cat->term_id;
+      }
     }
-  } ?>
-  <?php $args = array(
+    $args = array(
       'post_type' => 'post',
       'posts_per_page' => '4',
       'post__not_in' =>array( $post->ID ),
       'category__in' => $catkwds,
       'orderby' => 'rand'
     );
-  $my_query = new WP_Query( $args );?>
-    <?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
-      <li><a href="<?php the_permalink(); ?>">
-        <?php the_post_thumbnail('medium'); ?>
-        <div class="text">
+    $my_query = new WP_Query( $args );
+    while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+      <li>
+        <a href="<?php the_permalink(); ?>">
+          <?php
+          if ( has_post_thumbnail() ) {
+            the_post_thumbnail('medium');
+          } else {
+            ?>
+            <img width="300" height="300" src="<?php echo get_template_directory_uri() ?>/images/thumbnail-default.jpg" class="attachment-medium size-medium wp-post-image" alt="デフォルトのサムネイル">
+            <?php
+          } ?>
+          <div class="text">
           <?php the_title(); ?>
-        </div>
-      </a></li>
+          </div>
+        </a>
+      </li>
     <?php endwhile; ?>
   <?php wp_reset_postdata(); ?>
   </ul>
