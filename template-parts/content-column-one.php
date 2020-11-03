b84aebe3c7fcecd2c48d81b8c7ab887357efc7d1
taako-502 <i.am.armorknight@gmail.com>
2020-11-03 21:52:51 +0900

    記事全体メインエリアとサイドバーエリアのhtml修正

--- template-parts/content-column-one.php
+++ template-parts/content-column-one.php
@@ -1,20 +1,18 @@
 <section class="contents">
-  <div id="content-wrap" class="container">
-    <div id="main" class="col-md-12" >
+  <div class="l-main">
+    <?php
+    if ( have_posts() ) :
+      while ( have_posts() ) : the_post();
+      ?>
+      <h1><?php the_title();?></h1>
+      <section>
+        <?php the_content(); ?>
+      </section>
       <?php
-      if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
-        ?>
-        <h1><?php the_title();?></h1>
-        <section>
-          <?php the_content(); ?>
-        </section>
-        <?php
-          //関連記事
-          get_template_part( 'template-parts/content', 'related' );
-        endwhile;
-      endif;
-    ?>
-    </div>
+        //関連記事
+        get_template_part( 'template-parts/content', 'related' );
+      endwhile;
+    endif;
+  ?>
   </div>
 </section>
