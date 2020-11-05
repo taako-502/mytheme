e39bc981924aaaf06c530d2185a493c5e6817d63
taako <36348377+taako-502@users.noreply.github.com>
2020-11-05 22:46:09 +0900

    ampの設定子みっち

--- amp.php
+++ amp.php
@@ -5,6 +5,6 @@
  * @package mytheme
  */
 get_header("amp");
-get_template_part( 'template-parts/content', 'amp' );
+get_template_part( 'template-parts/content', 'column-two' );
 get_footer("amp");
 ?>
