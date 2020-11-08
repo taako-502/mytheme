32f1c83a0d6775970b1cb7d4f75d800e7f63e8e4
taako <36348377+taako-502@users.noreply.github.com>
2020-11-09 00:44:26 +0900

    ハンバーガメニューが多少動くように修正

--- js/main.js
+++ js/main.js
@@ -4,9 +4,5 @@ jQuery(document).ready(function($) {
       jQuery(this).toggleClass('active');
       jQuery('.l-nav-menu').toggleClass('active');
     });
-
-    jQuery('test').focusin(function(e) {
-      jQuery(this).css('background-color', '#ffc');
-    })
   })(jQuery);
 });
