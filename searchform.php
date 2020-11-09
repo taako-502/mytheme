8be8eb208a2915bcbf1160727749b9b4ccd140fc
taako <36348377+taako-502@users.noreply.github.com>
2020-11-09 23:50:29 +0900

    AMPのフォーム設定追加

--- searchform.php
+++ searchform.php
@@ -1,4 +1,17 @@
-<form method="get" class="searchform" action="<?php echo esc_url( home_url('/') ); ?>">
-  <input type="text" placeholder="キーワード検索" name="s" class="searchfield" value="" />
-  <input type="submit" value=""<?php echo isset($_GET['amp']) && $_GET['amp'] == 1 ?  "" : " alt=\"検索\""; ?> title="検索" class="searchsubmit">
-</form>
+<?php
+if(isset($_GET['amp']) && $_GET['amp'] == 1 ) {
+  //AMP ?>
+  <form method="get" class="searchform" action="<?php echo esc_url( home_url('/') ); ?>">
+    <fieldset>
+      <input type="text" placeholder="キーワード検索" name="s" class="searchfield" value="" />
+      <input type="submit" value="" title="検索" class="searchsubmit">
+    </fieldset>
+  </form>
+<?php
+} else {
+  //通常?>
+  <form method="get" class="searchform" action="<?php echo esc_url( home_url('/') ); ?>">
+    <input type="text" placeholder="キーワード検索" name="s" class="searchfield" value="" />
+    <input type="submit" value="" alt="検索" title="検索" class="searchsubmit">
+  </form>
+<?php } ?>
