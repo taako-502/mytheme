470009dc16692f0234143e4866b4fe3386c4fd23
taako <36348377+taako-502@users.noreply.github.com>
2020-11-09 20:09:28 +0900

    ハンバーガーメニュー🍔を追加

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
   (function($) {
     jQuery('.l-hamburger').click(function() {
       jQuery(this).toggleClass('active');
-      jQuery('.l-nav-menu').toggleClass('active');
+      jQuery('.l-nav').toggleClass('active');
     });
   })(jQuery);
 });
