0050e52f3b07abba5f86116db52b9214c10f0c71
taako <36348377+taako-502@users.noreply.github.com>
2020-11-10 00:21:38 +0900

    AMP用のタグ追加

--- searchform.php
+++ searchform.php
@@ -1,7 +1,10 @@
 <?php
 if(isset($_GET['amp']) && $_GET['amp'] == 1 ) {
   //AMP ?>
-  <form method="get" class="searchform" action="<?php echo esc_url( home_url('/') ); ?>">
+  <form method="get"
+      class="searchform"
+      action="<?php echo esc_url( home_url('/') ); ?>"
+      target="_blank ">
     <fieldset>
       <input type="text" placeholder="キーワード検索" name="s" class="searchfield" value="" />
       <input type="submit" value="" title="検索" class="searchsubmit">
@@ -10,11 +13,7 @@ if(isset($_GET['amp']) && $_GET['amp'] == 1 ) {
 <?php
 } else {
   //通常?>
-  <form
-    method="get"
-    class="searchform"
-    action="<?php echo esc_url( home_url('/') ); ?>"
-    target="_blank ">
+  <form method="get" class="searchform" action="<?php echo esc_url( home_url('/') ); ?>">
     <input type="text" placeholder="キーワード検索" name="s" class="searchfield" value="" />
     <input type="submit" value="" alt="検索" title="検索" class="searchsubmit">
   </form>
