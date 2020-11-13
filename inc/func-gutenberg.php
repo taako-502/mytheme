13998e30b23093bc73ee27b0e47b9b7d1d11a94e
taako <36348377+taako-502@users.noreply.github.com>
2020-11-13 23:12:13 +0900

    ブロックスタイル追加

--- inc/func-gutenberg.php
+++ inc/func-gutenberg.php
@@ -28,21 +28,39 @@
 <!-- /wp:columns --></div></div>
 <!-- /wp:group -->';
 
- register_block_pattern(
- 	'my-pattern/test',
+register_block_pattern(
+  'my-pattern/test',
  	array(
- 		'title'       => '生徒様の声ページ',
-     'description' => '顔写真、名前、感想の３点セット',
+ 	  'title'       => '生徒様の声ページ',
+    'description' => '顔写真、名前、感想の３点セット',
  		'content'     => $pattern,
- 	)
- );
+  )
+);
+
+/**
+ * カスタムエディタ追加
+ */
+function add_block_editor() {
+  wp_enqueue_style( 'gutenberg-style', get_stylesheet_directory_uri() . '/css/gutenberg.css' );
+  wp_enqueue_script( 'gutenberg-custom', get_stylesheet_directory_uri() . '/js/editor.js',array(), "", true);
+}
+add_action( 'enqueue_block_editor_assets', 'add_block_editor' );
 
 
- /**
-  * カスタムエディタ追加
-  */
- function add_my_assets_to_block_editor() {
-   wp_enqueue_style( 'block-style', get_stylesheet_directory_uri() . '/css/block_style.css' );
-   wp_enqueue_script( 'block-custom', get_stylesheet_directory_uri() . '/js/block_custom.js',array(), "", true);
- }
- add_action( 'enqueue_block_editor_assets', 'add_my_assets_to_block_editor' );
+add_theme_support( 'align-wide' );
+
+/**
+ * ブロックスタイルを追加
+ * @var [type]
+ */
+add_action( 'init', function() {
+
+ 	register_block_style(
+ 		'core/list',
+ 		[
+ 			'name' => 'ligthblue-bg',
+ 			'label' => '薄い青背景',
+ 			'inline_style' => '.is-style-black-bg { background: #f8f9ff; }',
+ 		]
+ 	);
+ } );
