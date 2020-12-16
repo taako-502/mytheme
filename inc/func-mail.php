df7a257daca6ec3488110211476497624a29afd9
taako-502 <i.am.armorknight@gmail.com>
2020-12-16 23:44:36 +0900

    テーブル定義をメールの項目に合わせる。

--- inc/func-mail.php
+++ inc/func-mail.php
@@ -143,11 +143,10 @@ function mailbox_create_table() {
 
   $charset_collate = $wpdb->get_charset_collate();
   $sql = "CREATE TABLE $table_name (
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
-    time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-    name tinytext NOT NULL,
-    text text NOT NULL,
-    url varchar(55) DEFAULT '' NOT NULL,
+    id mediumint(6) NOT NULL AUTO_INCREMENT,
+    name text NOT NULL,
+    mail_address text NOT NULL,
+    content text,
     UNIQUE KEY id (id)
   ) $charset_collate;";
   require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
