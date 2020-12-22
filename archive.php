dd9f1d719be43f20a3af2bf47f046ede63a52362
taako <36348377+taako-502@users.noreply.github.com>
2020-12-22 21:49:29 +0900

    タグの整合性や、class名などを修正

--- archive.php
+++ archive.php
@@ -1,29 +1,27 @@
 <?php get_header(); ?>
 <section class="contents">
-  <div class="l-main">
+  <main class="l-main">
     <h1>記事一覧</h1>
+    <?php
+    $category_name = get_the_category()[0]->cat_name;
+    $category_link = get_category_link(get_the_category()[0]->cat_ID);
+    ?>
+    <div class="p-category"><a href="<?php echo $category_link ?>"><?php echo $category_name; ?></a></div>
+    <div class="p-news--list">
       <?php
-      $category_name = get_the_category()[0]->cat_name;
-      $category_link = get_category_link(get_the_category()[0]->cat_ID);
-      echo "<div class=\"category\"><a href=\"$category_link\">".$category_name."</a></div>";
-      ?>
-      <hr>
-      <div class="p-news--list">
-        <?php
-        /* 記事一覧 */
-        if ( have_posts() ) {
-          while ( have_posts() ) {
-            the_post();
-            get_template_part('/template-parts/content','article');
-          }
-          the_posts_navigation();
-        } else {
-          echo "<p>まだ記事がありません。</p>";
+      /* 記事一覧 */
+      if ( have_posts() ) {
+        while ( have_posts() ) {
+          the_post();
+          get_template_part('/template-parts/content','article');
         }
-        ?>
-      </div>
+        the_posts_navigation();
+      } else {
+        echo "<p>まだ記事がありません。</p>";
+      }
+      ?>
     </div>
-    <?php get_sidebar(); ?>
-  </div>
+  </main>
+  <?php get_sidebar(); ?>
 </section>
 <?php get_footer(); ?>
