3071862824925405dce7c63611ae875e002de12d
taako−502 <i.am.armorknight@gmail.com>
2021-01-06 18:03:02 +0900

    カテゴリ名を取得できるよう改善

--- archive.php
+++ archive.php
@@ -3,12 +3,14 @@
   <main class="l-main">
     <h1>記事一覧</h1>
     <?php
-    if(isset(get_the_category()[0])){
-      $category_name = get_the_category()[0]->cat_name;
-      $category_link = get_category_link(get_the_category()[0]->cat_ID);
+    $cat_id = get_query_var('cat');
+    $cat = get_category($cat_id);
+    if(isset($cat)){
+      $cat_name = $cat->cat_name;
+      $cat_link = get_category_link($cat->term_id);
       ?>
       <div class="p-category">
-        <a href="<?php echo $category_link; ?>"><?php echo $category_name; ?></a>
+        <a href="<?php echo $cat_link; ?>"><?php echo $cat_name; ?></a>
       </div>
       <?php
     } ?>
