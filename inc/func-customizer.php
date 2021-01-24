706b025e13b3014c52d46d921dfd2b9d3de74b7f
taako−502 <i.am.armorknight@gmail.com>
2021-01-24 18:21:15 +0900

    ファイル分割

--- inc/func-customizer.php
+++ inc/func-customizer.php
@@ -1,6 +1,21 @@
 <?php
 //カスタムヘッダー
 get_template_part('/inc/custom', 'header');
+get_template_part('/inc/custom', 'nav');
+
+/**
+ * JavaScript のエンキュー
+ * @see add_action('customize_preview_init',$func)
+ */
+function mytheme_customizer_live_preview(){
+  wp_enqueue_script(
+    'mytheme-themecustomizer',  //このスクリプトの ID 名
+    get_template_directory_uri().'/js/mytheme-customizer.js', // このファイルの URL
+    array( 'jquery','customize-preview' ),  //　必須の依存ファイル
+    true // フッターに出力する場合 true にします
+  );
+}
+add_action( 'customize_preview_init', 'mytheme_customizer_live_preview' );
 
 /**
  * カスタマイザー
@@ -19,59 +34,3 @@ add_action( 'customize_register', 'mytheme_customize' );
  */
 function getCusNavTextColor(){ return get_theme_mod('nav_text_color','#FFF'); }
 function getCusNavBackColor(){ return get_theme_mod('nav_background_color','#212529'); }
-
-/**
- * ナビゲーションのカスタマイズを行うメソッド
- * @param  WP_Customize_Manager $wp_customize カスタマイズの設定
- */
-function cusNav( $wp_customize ) {
-  // ナビゲーションバーの色設定
-  $wp_customize->add_panel(
-    'nav',
-    array(
-      'title'    => 'ナビゲーション',
-      'priority' => 21,
-    )
-  );
-
-  $wp_customize->add_section(
-    'nav-color',
-    array(
-      'title'    => 'カラー',
-      'panel'    => 'nav',
-      'priority' => 1,
-    )
-  );
-
-  $wp_customize->add_setting( 'nav_text_color' , array(
-    'default'    => '#FFF',
-    'sanitize_callback' => 'sanitize_hex_color',
-  ));
-  $wp_customize->add_control(
-    new WP_Customize_Color_Control(
-      $wp_customize,
-      'ctl_nav_text_color',
-      array(
-        'label'    => 'ナビの文字色',
-        'section'  => 'nav-color',
-        'settings' => 'nav_text_color',
-      )
-    )
-  );
-
-  $wp_customize->add_setting( 'nav_background_color' , array(
-    'default'    => '#333',
-    'sanitize_callback' => 'sanitize_hex_color',
-  ));
-  $wp_customize->add_control(
-    new WP_Customize_Color_Control(
-      $wp_customize,
-      'ctl_nav_background_color',
-      array(
-        'label'    => 'ナビバーの背景色',
-        'section'  => 'nav-color',
-        'settings' => 'nav_background_color',
-      )
-    )
-  );
-}
---
+++ js/mytheme-customizer.js
@@ -0,0 +1,42 @@
+/**
+ * このファイルはテーマカスタマイザーライブプレビューをライブにします。
+ * テーマ設定で 'postMessage' を指定し、ここで制御します。
+ * JavaScript はコントロールからテーマ設定を取得し、
+ * jQuery を使ってページに変更を反映します。
+ */
+(function($) {
+  // リアルタイムにサイトタイトルを変更
+  wp.customize('blogname', function(value) {
+    value.bind(function(newval) {
+      $('#site-title a').html(newval);
+    });
+  });
+  // リアルタイムにキャッチフレーズを変更
+  wp.customize('blogdescription', function(value) {
+    value.bind(function(newval) {
+      $('.site-description').html(newval);
+    });
+  });
+
+  // リアルタイムにサイトタイトルの色を変更
+  wp.customize('header_textcolor', function(value) {
+    value.bind(function(newval) {
+      $('#site-title a').css('color', newval);
+    });
+  });
+
+  // リアルタイムに背景色を変更
+  wp.customize('background_color', function(value) {
+    value.bind(function(newval) {
+      $('body').css('background-color', newval);
+    });
+  });
+
+  // リアルタイムにリンク色を変更
+  wp.customize('mytheme_options[link_textcolor]', function(value) {
+    value.bind(function(newval) {
+      $('a').css('color', newval);
+    });
+  });
+
+})(jQuery);
