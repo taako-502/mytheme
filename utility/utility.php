b6614a88a69fa4a7472401db2fb715ea7c17032d
taako−502 <i.am.armorknight@gmail.com>
2021-01-24 20:08:06 +0900

    変数名のミスを修正

--- utility/utility.php
+++ utility/utility.php
@@ -1,12 +1,12 @@
 <?php
 namespace ut;
 
+/**
+* 引数がnullか空白か判定
+* @param  [type]  $val [description]
+* @return boolean      nullか空白ならtrueを返却
+*/
 if (!function_exists('isNullOrEmpty')) {
-  /**
-  * 引数がnullか空白か判定
-  * @param  [type]  $val [description]
-  * @return boolean      nullか空白ならtrueを返却
-  */
   function isNullOrEmpty($obj) {
     if($obj === 0 || $obj === "0") {
       return false;
@@ -15,12 +15,12 @@ if (!function_exists('isNullOrEmpty')) {
   }
 }
 
+/**
+* 引数がnullかスペースか判定
+* @param  [type]  $obj [description]
+* @return boolean      nullかスペースならtrueを返却
+*/
 if (!function_exists('isNullOrWhitespace')) {
-  /**
-   * 引数がnullかスペースか判定
-   * @param  [type]  $obj [description]
-   * @return boolean      nullかスペースならtrueを返却
-   */
   function isNullOrWhitespace($obj) {
     if(isNullOrEmpty($obj) === true){
       return true;
@@ -38,8 +38,10 @@ if (!function_exists('isNullOrWhitespace')) {
  * @param  String $default デフォルト値
  * @return String          値（空白の場合デフォルト値）
  */
-function getValOrDef( $val, $default){
-  return empty(trim($var)) ? $default : $var ;
+if (!function_exists('getValOrDef')) {
+  function getValOrDef( $val, $default){
+    return isNullOrWhitespace($val) ? $default : $val ;
+  }
 }
 
 /**
@@ -48,11 +50,13 @@ function getValOrDef( $val, $default){
  * @param  Integer $len     文字数
  * @return String           ディスクリプション
  */
-function getDescription($id,$len){
-  $description = get_post($id)->post_content;
-  $description = str_replace(array("\r\n","\r","\n","&nbsp;"),'',$description);
-  $description = wp_strip_all_tags($description);
-  $description = preg_replace('/\[.*\]/','',$description);
-  return mb_strimwidth($description,0,$len,"...");
+if (!function_exists('getDescription')) {
+  function getDescription($id,$len){
+    $description = get_post($id)->post_content;
+    $description = str_replace(array("\r\n","\r","\n","&nbsp;"),'',$description);
+    $description = wp_strip_all_tags($description);
+    $description = preg_replace('/\[.*\]/','',$description);
+    return mb_strimwidth($description,0,$len,"...");
+  }
 }
 ?>
