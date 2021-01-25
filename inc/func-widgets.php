522f16945c341cc4e9124964e3c1aa60223be557
taako−502 <i.am.armorknight@gmail.com>
2021-01-25 21:18:38 +0900

    サイドバーのデザイン調整

--- inc/func-widgets.php
+++ inc/func-widgets.php
@@ -1,12 +1,13 @@
 <?php
-add_action(
-	'widgets_init',
-	function(){
-		register_sidebar(array(
-			'id' => 'widget_sidebar001',
-			'name' => 'サイドバー',
-			'description' => 'サイドバーのウィジェット',
-		));
-	}
-);
+function mytheme_register_sidebars(){
+	register_sidebar(array(
+		'id' => 'widget_sidebar001',
+		'name' => 'サイドバー',
+		'description' => 'サイドバーのウィジェット',
+		'before_widget' => '<div class="l-sidebar--widget">', //ウィジェットを囲う開始タグ
+		'after_widget'  => '</div>', //ウィジェットを囲う終了タグ
+		'before_title'  => '<h4 class="l-sidebar--widgettitle">', //タイトルを囲う開始タグ
+		'after_title'   => '</h4>', //タイトルを囲う終了タグ
+	));
+add_action( 'widgets_init' , 'mytheme_register_sidebars' );
 ?>
