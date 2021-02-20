618e14119390db071a331cbf22e7424ce4c514c2
taako-502 <i.am.armorknight@gmail.com>
2021-02-20 23:40:52 +0900

    レンジスライダー改善

--- class/WP_Customize_Range.php
+++ class/WP_Customize_Range.php
@@ -26,7 +26,7 @@ class WP_Customize_Range extends WP_Customize_Control {
     <label>
       <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
       <input class='range-slider' min="<?php echo $this->min ?>" max="<?php echo $this->max ?>" step="<?php echo $this->step ?>" type='range' <?php $this->link(); ?> value="<?php echo esc_attr( $this->value() ); ?>" oninput="jQuery(this).next('input').val( jQuery(this).val() )">
-      <input onKeyUp="jQuery(this).prev('input').val( jQuery(this).val() )" type='text' value='<?php echo esc_attr( $this->value() ); ?>'>
+      <input onKeyUp="jQuery(this).prev('input').val( jQuery(this).val() )" type='number' min="<?php echo $this->min ?>" max="<?php echo $this->max ?>" step="<?php echo $this->step; ?>" value='<?php echo esc_attr( $this->value() ); ?>'>
     </label>
     <?php
   }
