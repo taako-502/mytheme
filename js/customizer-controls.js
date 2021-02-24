b24e02d2ec890b53994364687f4704938566a680
taako-502 <i.am.armorknight@gmail.com>
2021-02-25 00:22:59 +0900

    初期処理時に可視性制御が効かない問題の改善

--- js/customizer-controls.js
+++ js/customizer-controls.js
@@ -6,42 +6,62 @@
 jQuery(document).ready(function($) {
 	(function ($) {
 		wp.customize.bind('ready', function () {
+
+			/**
+			 *メインエリアの横幅を変更する項目の可視性制御
+			 * @param  {[type]} val [description]
+			 * @return {[type]}     [description]
+			 */
+			function oneColWithVisible(val){
+				if(val === "one-col" ){
+					jQuery("#_customize-input-ctl_architect_col_one_width").prop('disabled', false);
+				} else {
+					jQuery("#_customize-input-ctl_architect_col_one_width").prop('disabled', true);
+				}
+			}
 			/* 1カラムの時のみ、メインエリアの横幅を設定可能 */
 			wp.customize('front_architect_col', function (value) {
+				oneColWithVisible(value.get());
 				value.bind(function (newval) {
-					if(newval === "one-col" ){
-						jQuery("#_customize-input-ctl_architect_col_one_width").prop('disabled', false);
-					} else {
-						jQuery("#_customize-input-ctl_architect_col_one_width").prop('disabled', true);
-					}
+					oneColWithVisible(newval);
 				});
 			});
+
+			/**
+			 * スライダーの可視性制御
+			 * @param  {[type]} newval [description]
+			 * @return {[type]}        [description]
+			 */
+			function sliderTypeVisible(val){
+				if(val != "recommend" ){
+					jQuery("#customize-control-ctl_front_slider_url_1").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_2").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_3").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_4").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_5").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_6").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_7").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_8").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_9").css('display', 'none');
+					jQuery("#customize-control-ctl_front_slider_url_10").css('display', 'none');
+				} else {
+					jQuery("#customize-control-ctl_front_slider_url_1").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_2").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_3").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_4").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_5").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_6").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_7").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_8").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_9").css('display', 'block');
+					jQuery("#customize-control-ctl_front_slider_url_10").css('display', 'block');
+				}
+			}
 			/* おすすめ記事選択時のみ表示 */
 			wp.customize('front_slider_type', function (value) {
+				sliderTypeVisible(value.get());
 				value.bind(function (newval) {
-					if(newval != "recommend" ){
-						jQuery("#customize-control-ctl_front_slider_url_1").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_2").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_3").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_4").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_5").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_6").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_7").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_8").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_9").css('display', 'none');
-						jQuery("#customize-control-ctl_front_slider_url_10").css('display', 'none');
-					} else {
-						jQuery("#customize-control-ctl_front_slider_url_1").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_2").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_3").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_4").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_5").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_6").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_7").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_8").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_9").css('display', 'block');
-						jQuery("#customize-control-ctl_front_slider_url_10").css('display', 'block');
-					}
+					sliderTypeVisible(newval);
 				});
 			});
 		});
