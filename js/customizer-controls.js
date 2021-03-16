d3bf8039be7264977a725374ff2713a9bf99ab78
taako-502 <i.am.armorknight@gmail.com>
2021-03-17 00:09:24 +0900

    セレクタを変更

--- js/customizer-controls.js
+++ js/customizer-controls.js
@@ -14,9 +14,9 @@ jQuery(document).ready(function($) {
        */
       function oneColWithVisible(val) {
         if (val === "one-col") {
-          jQuery("#_customize-input-ctl_architect_col_one_width").prop('disabled', false);
+          jQuery("#_customize-input-mytheme_settings-architect_col_one_width").prop('disabled', false);
         } else {
-          jQuery("#_customize-input-ctl_architect_col_one_width").prop('disabled', true);
+          jQuery("#_customize-input-mytheme_settings-architect_col_one_width").prop('disabled', true);
         }
       }
       /* 1カラムの時のみ、メインエリアの横幅を設定可能 */
@@ -34,27 +34,27 @@ jQuery(document).ready(function($) {
        */
       function sliderTypeVisible(val) {
         if (val !== "recommend") {
-          jQuery("#customize-control-ctl_parts_header_slider_url_1").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_2").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_3").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_4").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_5").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_6").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_7").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_8").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_9").addClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_10").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_1").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_2").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_3").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_4").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_5").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_6").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_7").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_8").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_9").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_10").addClass('u-display__none');
         } else {
-          jQuery("#customize-control-ctl_parts_header_slider_url_1").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_2").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_3").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_4").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_5").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_6").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_7").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_8").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_9").removeClass('u-display__none');
-          jQuery("#customize-control-ctl_parts_header_slider_url_10").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_1").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_2").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_3").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_4").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_5").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_6").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_7").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_8").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_9").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_10").removeClass('u-display__none');
         }
       }
       /* おすすめ記事選択時のみ表示 */
@@ -65,7 +65,6 @@ jQuery(document).ready(function($) {
         });
       });
 
-
 			/**
        * [sliderAutoVisible description]
        * @param  {[type]} val [description]
@@ -73,9 +72,9 @@ jQuery(document).ready(function($) {
        */
       function sliderAutoVisible(val) {
         if (val === true) {
-          jQuery("#customize-control-ctl_parts_header_slider_auto_speed").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_auto_speed").removeClass('u-display__none');
         } else {
-          jQuery("#customize-control-ctl_parts_header_slider_auto_speed").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_auto_speed").addClass('u-display__none');
         }
       }
 
@@ -92,14 +91,14 @@ jQuery(document).ready(function($) {
        * @return {[type]}        [description]
        */
       function sliderAooNumberVisible(val) {
-        if (jQuery("#customize-control-ctl_parts_header_slider_url_1").hasClass('u-display__none')) {
+        if (jQuery("#customize-control-mytheme_settings-parts_header_slider_url_1").hasClass('u-display__none')) {
           return;
         }
         for (var i = 1; i <= 10; i++) {
-          jQuery("#customize-control-ctl_parts_header_slider_url_" + i).removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_" + i).removeClass('u-display__none');
         }
         for (var i = 10; i > val; i--) {
-          jQuery("#customize-control-ctl_parts_header_slider_url_" + i).addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_url_" + i).addClass('u-display__none');
         }
       }
       /* おすすめ記事選択時のみ表示 */
@@ -117,9 +116,9 @@ jQuery(document).ready(function($) {
        */
       function sliderWidthMaxwindowVisible(val) {
         if (val === "px") {
-          jQuery("#customize-control-ctl_parts_header_slider_width").removeClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_width").removeClass('u-display__none');
         } else {
-          jQuery("#customize-control-ctl_parts_header_slider_width").addClass('u-display__none');
+          jQuery("#customize-control-mytheme_settings-parts_header_slider_width").addClass('u-display__none');
         }
       }
       /* おすすめ記事選択時のみ表示 */
